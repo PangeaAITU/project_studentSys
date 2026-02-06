92622d3251f16a04521f432accc898b4135d9bba
Ruinch <163226394+Ruinch@users.noreply.github.com>
2026-02-06 07:18:02 +0500

    db change

--- sms/settings.py
+++ sms/settings.py
@@ -4,6 +4,7 @@ Django settings for sms project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,10 +68,9 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 # Password validation
