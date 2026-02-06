1c1377b9b9ad198265765da605c075aeb9db04a0
Ruinch <163226394+Ruinch@users.noreply.github.com>
2026-02-06 09:09:31 +0500

    Update settings.py

--- sms/settings.py
+++ sms/settings.py
@@ -69,7 +69,8 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
