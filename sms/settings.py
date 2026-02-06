e6fe6d76e144b7dccfc45d3dcd3214168be84105
Ruinch <163226394+Ruinch@users.noreply.github.com>
2026-02-06 06:53:46 +0500

    fix

--- sms/settings.py
+++ sms/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sms.urls'
@@ -144,3 +145,9 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
